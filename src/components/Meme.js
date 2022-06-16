c02093edabfc4b9fce9067286534acac91063bde
Asitha Navaratne <asithan476@gmail.com>
2022-06-16 20:57:14 +0530

    Get random url from data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,22 @@
+import memeData from "../memeData";
+
 function Meme() {
+  function getMemeData() {
+    const memeArray = memeData["data"]["memes"];
+    const randomNumber = Math.round(Math.random() * memeArray.length);
+    const { url } = memeArray[randomNumber];
+    console.log(url);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form--input" placeholder="Top Text" />
         <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button className="form--button">Get a new meme image 😜</button>
-      </form>
+        <button onClick={getMemeData} className="form--button">
+          Get a new meme image 😜
+        </button>
+      </div>
     </main>
   );
 }
