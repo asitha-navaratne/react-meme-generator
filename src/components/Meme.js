6c51389ca1b8abe133ac3e6eb2b55e8daaf40035
Asitha Navaratne <asithan476@gmail.com>
2022-06-21 23:43:40 +0530

    Adding text to the image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,24 +2,56 @@ import { useState } from "react";
 import memeData from "../memeData";
 
 function Meme() {
-  const [memeImage, setMemeImage] = useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages] = useState(memeData);
 
-  function getMemeData() {
-    const memeArray = memeData["data"]["memes"];
+  function getMeme() {
+    const memeArray = allMemeImages["data"]["memes"];
     const randomNumber = Math.round(Math.random() * memeArray.length);
     const { url } = memeArray[randomNumber];
-    setMemeImage(url);
+    setMeme((prevMeme) => ({ ...prevMeme, randomImage: url }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prev) => ({ ...prev, [name]: value }));
   }
 
   return (
     <main>
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top Text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
-        <button onClick={getMemeData} className="form--button">
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+        <button onClick={getMeme} className="form--button">
           Get a new meme image 😜
         </button>
-        <img src={memeImage} alt="meme template" className="meme--image" />
+        <div className="meme">
+          <img
+            src={meme.randomImage}
+            alt="meme template"
+            className="meme--image"
+          />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
       </div>
     </main>
   );
